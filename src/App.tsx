c6867d460be4d41b6839da4bcac24c268726fc7e
diego breeg <diegobreeg@gmail.com>
2024-09-25 00:26:17 -0300

    header with navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import './App.css'
-import { Header } from './components/Header'
+import { Header } from './components/Header';
+import styles from './App.module.css';
 
-function App()
-{
-
-  return (
-    <>
-      <Header />
-    </>
-  )
+function App() {
+    return (
+        <div className={styles.layout}>
+            <Header />
+        </div>
+    );
 }
 
-export default App
+export default App;
