97ccaed4a6e425acfac1d2c82d30a87f532ffcc6
diego breeg <diegobreeg@gmail.com>
2024-09-25 13:47:04 -0300

    architectural adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { Header } from './components/Header';
-import styles from './App.module.css';
+
 
 function App() {
     return (
-        <div className={styles.layout}>
-            <Header />
-        </div>
+        <>
+           
+        </>
     );
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,8 @@
-import styles from './Header.module.css';
-import logo from '../../assets/images/logo.png'; // Ajuste o caminho conforme necessário
 
 export function Header() {
     return (
-        <header className={styles.header}>
-            <div className={styles.left}>
-                <img src={logo} alt='Logo' className={styles.logo} />
-            </div>
-            <div className={styles.middle}>
-                <input type="text" placeholder="Buscar..." className={styles.searchbar} />
-            </div>
-            <nav className={styles.right}>
-                <a href="/">Home</a>
-                <a href="/produtos">Produtos</a>
-                <a href="/sobre">Sobre</a>
-                <a href="/contato">Contato</a>
-            </nav>
-        </header>
+       <header>
+
+       </header>
     );
 }
