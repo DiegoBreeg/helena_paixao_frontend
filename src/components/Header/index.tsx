cedf7c21025a3519c5df2d253443d4e1ac421114
diegobreeg <diegobreeg@gmail.com>
2024-09-26 10:12:37 -0300

    add-component Navbelt

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
+import { Navbelt } from '../Navbelt';
 import styles from './Header.module.css'
 
 
 export function Header() {
     return (
        <header className={styles.header}>
-            header
+            <Navbelt />
        </header>
     );
 }
---
+++ src/components/Navbelt/index.tsx
@@ -0,0 +1,8 @@
+import styles from "./Navbelt.module.css"
+
+export function Navbelt()
+{
+    return (
+        <div className={styles.navbelt} />
+    )
+}
