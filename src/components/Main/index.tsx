580cb43557b76f291d1283c62b65e1db99c31711
diego breeg <diegobreeg@gmail.com>
2024-10-20 11:29:28 -0300

    update

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,17 +1,20 @@
+import { ReactElement } from 'react'
 import { ProductCard } from '../ProductCard'
 import styles from './Main.module.css'
 
 export function Main()
 {
+    const products: ReactElement[] =
+        [
+            <ProductCard />,
+            <ProductCard />,           
+            
+        ]
+
+
     return (
         <main className={styles.main}>
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />                   
-            <ProductCard />                   
-            <ProductCard />                   
-            <ProductCard />                   
-            <ProductCard />
+            {products}
         </main>
     )
 }
